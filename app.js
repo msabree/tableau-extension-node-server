9b07ada8afa3cda9c894f99dbc0e0092d4d47867
Makeen Sabree <makeen.sabree@gmail.com>
2019-12-15 21:54:58 -0500

    Setting up server to accept a file upload and write to server.

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8081;
+const options = {
+    inflate: true,
+    limit: '500kb',
+    type: 'application/octet-stream'
+};
+
+app.use(cors());
+app.use(bodyParser.raw(options));
+
+app.put('/writeback', (req, res) => {
+    fs.writeFile('data_source_updated.xlsx', req.body, (err) => {
+        if(err){
+            console.log(err);
+            res.send('Write back failed!');
+        }
+        else{
+            res.send('Write back succeeded!');
+        }
+        
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
